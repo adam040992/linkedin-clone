c41141006ebad1979385507cbbd1d56de6c716c7
adam040992 <adam040992@gmail.com>
2021-06-05 21:33:21 +0200

    Build: Header

--- src/components/Login.js
+++ src/components/Login.js
@@ -8,13 +8,82 @@ function Login() {
                 <a href="/">
                     <img src="/images/login-logo.svg" alt="Login Logo" />
                 </a>
+                <div>
+                    <Join>Join now</Join>
+                    <SignIn>Sign in</SignIn>
+                </div>
             </Nav>
+
+            <Section>
+                Section
+            </Section>
         </Container>
     )
 }
 
 export default Login
 
-const Container = styled.div``;
+const Container = styled.div`
+    padding: 0px;
+`;
 
-const Nav = styled.div``;
+const Nav = styled.div`
+    max-width: 1128px;
+    margin: auto;
+    padding: 12px 5px 16px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    position: relative;
+    flex-wrap: nowrap;
+
+    & > a {
+        width: 135px;
+        height: 35px;
+        @media (max-width: 768px) {
+            padding: 0 5px;
+        }
+    }
+`;
+
+const Join = styled.a`
+    font-size: 16px;
+    padding: 10px 12px;
+    text-decoration: none;
+    border-radius: 4px;
+    color: rgba(0, 0, 0, 0.6);
+    margin-right: 12px;
+    cursor: pointer;
+
+    &:hover {
+        background-color: rgba(0, 0, 0, 0.08);
+        color: rgba(0, 0, 0, 0.9);
+        text-decoration: none;
+    }
+`;
+
+const SignIn = styled.a`
+    cursor: pointer;
+    box-shadow: inset 0 0 0 1px #0a66c2;
+    color: #0a66c2;
+    border-radius: 24px;
+    transition-duration: 167ms;
+    font-size: 16px;
+    font-weight: 600;
+    line-height: 40px;
+    padding: 10px 24px;
+    text-align: center;
+    background-color: rgba(0, 0, 0, 0);
+
+    &:hover {
+        background-color: rgba(112, 181, 249, 0.15);
+        color: #0a66c2;
+        text-decoration: none;
+    }
+`;
+
+const Section = styled.section`
+    display: flex;
+    align-content: start;
+    min-height: 700px;
+`;
