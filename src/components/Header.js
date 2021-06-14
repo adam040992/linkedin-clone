6a41c37d35d584f98e64a6ef241434625eb42544
adam040992 <adam040992@gmail.com>
2021-06-14 21:09:09 +0200

    Header building

--- src/components/Header.js
+++ src/components/Header.js
@@ -18,6 +18,16 @@ function Header() {
                         <img src="/images/search-icon.svg" alt="" />
                     </SearchIcon>
                 </Search>
+                <Nav>
+                    <NavListWrap>
+                        <NavList>
+                            <a>
+                                <img src="/images/nav-home.svg" alt="nav-home" />
+                                <span>Home</span>
+                            </a>
+                        </NavList>
+                    </NavListWrap>
+                </Nav>
             </Content>
         </Container>
     )
@@ -84,4 +94,46 @@ const SearchIcon = styled.div`
     display: flex;
     justify-content: center;
     align-items: center;
+`;
+
+const Nav = styled.nav`
+    margin-left: auto;
+    display: block;
+    @media (max-width: 768px) {
+        position: fixed;
+        left: 0;
+        bottom: 0;
+        background: white;
+        width: 100%;
+    }
+`;
+
+const NavListWrap = styled.ul`
+    display: flex;
+    flex-wrap: wrap;
+    list-style-type: none;
+`;
+
+const NavList = styled.li`
+    display: flex;
+    align-items: center;
+
+    a {
+        align-items: center;
+        background: transparent;
+        display: flex;
+        flex-direction: column;
+        font-size: 12px;
+        font-weight: 400;
+        justify-content: center;
+        line-height: 1.5;
+        min-height: 42px;
+        min-width: 80px;
+        position: relative;
+        text-decoration: none;
+
+        span {
+            color: rgba(0, 0, 0, 0.6);
+        }
+    }
 `;
