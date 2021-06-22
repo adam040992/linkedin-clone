960ee42b9c4b417e93a937cf3ccef8470eedf939
adam040992 <adam040992@gmail.com>
2021-06-22 18:34:36 +0200

    Add: Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -20,12 +20,62 @@ function Header() {
                 </Search>
                 <Nav>
                     <NavListWrap>
-                        <NavList>
+                        <NavList className="active">
                             <a>
                                 <img src="/images/nav-home.svg" alt="nav-home" />
                                 <span>Home</span>
                             </a>
                         </NavList>
+
+                        <NavList>
+                            <a>
+                                <img src="/images/nav-network.svg" alt="nav-network" />
+                                <span>My Network</span>
+                            </a>
+                        </NavList>
+
+                        <NavList>
+                            <a>
+                                <img src="/images/nav-jobs.svg" alt="nav-jobs" />
+                                <span>Jobs</span>
+                            </a>
+                        </NavList>
+
+                        <NavList>
+                            <a>
+                                <img src="/images/nav-messaging.svg" alt="nav-messaging" />
+                                <span>Messaging</span>
+                            </a>
+                        </NavList>
+
+                        <NavList>
+                            <a>
+                                <img src="/images/nav-notifications.svg" alt="nav-home" />
+                                <span>Notofications</span>
+                            </a>
+                        </NavList>
+
+                        <User>
+                            <a>
+                                <img src="/images/user.svg" alt="user" />
+                                <span>Me
+                                <img src="/images/down-icon.svg" alt="down-icon" />
+                                </span>
+                            </a>
+
+                            <SignOut>
+                                <a>Sign Out</a>
+                            </SignOut>
+                        </User>
+
+                        <Work>
+                            <a>
+                                <img src="/images/nav-work.svg" alt="work-image" />
+                                <span>Work
+                                <img src="/images/down-icon.svg" alt="down-icon" />
+                                </span>
+                            </a>
+                        </Work>
                     </NavListWrap>
                 </Nav>
             </Content>
@@ -112,6 +162,22 @@ const NavListWrap = styled.ul`
     display: flex;
     flex-wrap: wrap;
     list-style-type: none;
+
+    .active {
+        span:after {
+            content: "";
+            transform: scaleX(1);
+            border-bottom: 2px solid var(--white, #fff);
+            bottom: 0;
+            left: 0;
+            right: 0;
+            top: 0;
+            position: absolute;
+            transition: transform 0.2s easy-in-out;
+            width: 100%;
+            border-color: rgba(0, 0, 0, 0.9);
+        }
+    }
 `;
 
 const NavList = styled.li`
@@ -134,6 +200,63 @@ const NavList = styled.li`
 
         span {
             color: rgba(0, 0, 0, 0.6);
+            display: flex;
+            align-items: center;
+        }
+
+        @media (max-width: 768px) {
+            min-width: 70px;
         }
     }
+
+    &:hover, &:active {
+        a {
+            span {
+                color: rgba(0, 0, 0, 0.9);
+            }
+        }
+    }
+`;
+
+const SignOut = styled.div`
+    position: absolute;
+    top: 45px;
+    background: white;
+    border-radius: 0 0 5px 5px;
+    width: 100px;
+    height: 40px;
+    font-size: 16px;
+    transition-duration: 167ms;
+    text-align: center;
+    display: none;
+`;
+
+const User = styled(NavList)`
+    a > svg {
+        width: 24px;
+        border-radius: 50%;
+    }
+
+    a > img {
+        width: 24px;
+        height: 24px;
+        border-radius: 50%;
+    }
+
+    span {
+        display: flex;
+        align-items: center;
+    }
+
+    &:hover {
+        ${SignOut} {
+            align-items: center;
+            display: flex;
+            justify-content: center;
+        }
+    }
+`;
+
+const Work = styled(User)`
+    border-left: 1px solid rgba(0, 0, 0, 0.08);
 `;
